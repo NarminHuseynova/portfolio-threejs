bdd7c3a4ad6eac38f7004e7fc8b87d266b65c8e0
Narmin Huseynova <90983090+NarminHuseynova@users.noreply.github.com>
2023-05-03 13:42:39 +0400

    added Test component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,22 @@
-import { ComputersCanvas } from "./canvas";
 import { styles } from "../styles";
+import Test from "./Test";
 
 const Hero = () => {
   return (
-    <section className={`relative w-full h-screen mx-auto`}>
+    <section
+      className={`relative w-full lg:h-screen mx-auto flex flex-row gap-5 justify-end h-[94vh] sm:h-[100vh] md:h-[130vh]`}
+    >
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex lg:flex-row flex-col items-center lg:items-start gap-5`}
       >
-        <div className="flex flex-col justify-center items-center mt-5">
+        <div className="lg:flex flex-col justify-center items-center mt-5 hidden">
           <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">Narmin</span>
+            Hi, <br /> I'm <span className="text-[#915EFF]">Narmin</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I developing websites, user interfaces and{" "}
@@ -22,9 +24,8 @@ const Hero = () => {
             web applications.
           </p>
         </div>
+        <Test />
       </div>
-
-      <ComputersCanvas />
     </section>
   );
 };
